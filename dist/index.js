93b1fae42ffe40daa73f3d7aa52ac988f7040441
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-10-23 09:35:13 -0400

    commit

--- dist/index.js
+++ dist/index.js
@@ -108,6 +108,7 @@ const path = __importStar(__nccwpck_require__(5622));
 const api_1 = __nccwpck_require__(8947);
 const const_1 = __nccwpck_require__(6695);
 const utils_1 = __nccwpck_require__(918);
+const qpmf_1 = __nccwpck_require__(3072);
 function downloadQpm(octokit, token) {
     return __awaiter(this, void 0, void 0, function* () {
         const artifacts = yield octokit.rest.actions.listArtifactsForRepo({
@@ -176,6 +177,11 @@ function run() {
             if (parameters.cache) {
                 yield cache.saveCache(paths, cacheKey !== null && cacheKey !== void 0 ? cacheKey : key);
             }
+            if (parameters.version) {
+                const qpm = yield (0, qpmf_1.readQPM)('./qpm.json');
+                qpm.info.version = parameters.version;
+                (0, qpmf_1.writeQPM)('qpm.json', qpm);
+            }
             // const ms: string = core.getInput('milliseconds')
             // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
             // core.debug(new Date().toTimeString())
@@ -190,6 +196,82 @@ function run() {
     });
 }
 run();
+
+
+/***/ }),
+
+/***/ 3072:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.writeQPM = exports.readQPM = void 0;
+const fs = __importStar(__nccwpck_require__(5747));
+function readQPM(file) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const fileData = yield new Promise((resolve, reject) => {
+            fs.readFile(file, undefined, (err, data) => {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve(data.toString());
+                }
+            });
+        });
+        return JSON.parse(fileData);
+    });
+}
+exports.readQPM = readQPM;
+function writeQPM(file, qpm) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve, reject) => {
+            const qpmStr = JSON.stringify(qpm);
+            fs.writeFile(file, qpmStr, err => {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+    });
+}
+exports.writeQPM = writeQPM;
 
 
 /***/ }),
